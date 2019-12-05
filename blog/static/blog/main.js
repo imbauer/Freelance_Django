cd644df7c96701212aa4800333bd1eb8cb1e81f5
imbauer <ivanbauer16@gmail.com>
2019-12-05 05:19:57 -0500

    Testing message

--- blog/static/blog/main.js
+++ blog/static/blog/main.js
@@ -3,12 +3,15 @@ function increaseValue() {
   value = isNaN(value) ? 2 : value;
   value++;
   document.getElementById('number').value = value;
-  document.getElementById('dynamic').innerHTML = value;
+  var count = 24.90 + (4.95 * (value - 3))
+  var count = count.toString();
+  var numarray = count.split('.')
+  document.getElementById('dynamic').innerHTML = count;
 }
 
 function decreaseValue() {
   var value = parseInt(document.getElementById('number').value, 10);
-  value = isNaN(value) ? 2 : value;
+  value = isNaN(value) ? 3 : value;
   value < 3 ? value = 3 : '';
   value--;
   document.getElementById('number').value = value;
