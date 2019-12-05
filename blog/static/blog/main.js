468cd272417605a6e16ffbfcd7ae9900790a6a99
imbauer <ivanbauer16@gmail.com>
2019-12-05 05:07:57 -0500

    Testing message

--- blog/static/blog/main.js
+++ blog/static/blog/main.js
@@ -1,6 +1,6 @@
 function increaseValue() {
   var value = parseInt(document.getElementById('number').value, 10);
-  value = isNaN(value) ? 0 : value;
+  value = isNaN(value) ? 2 : value;
   value++;
   document.getElementById('number').value = value;
   document.getElementById('dynamic').innerHTML = value;
@@ -8,8 +8,8 @@ function increaseValue() {
 
 function decreaseValue() {
   var value = parseInt(document.getElementById('number').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value < 1 ? value = 1 : '';
+  value = isNaN(value) ? 2 : value;
+  value < 3 ? value = 3 : '';
   value--;
   document.getElementById('number').value = value;
   document.getElementById('dynamic').innerHTML = value;
