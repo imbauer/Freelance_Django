436513a922480a21b9e82f27a9b1a281687a96bc
imbauer <ivanbauer16@gmail.com>
2019-12-08 23:59:18 -0500

    Testing message

--- blog/hooks.py
+++ blog/hooks.py
@@ -1,37 +1,37 @@
-from paypal.standard.models import ST_PP_COMPLETED
-from paypal.standard.ipn.signals import valid_ipn_received
-
-def show_me_the_money(sender, **kwargs):
-    ipn_obj = sender
-    if ipn_obj.payment_status == ST_PP_COMPLETED:
-        # WARNING !
-        # Check that the receiver email is the same we previously
-        # set on the `business` field. (The user could tamper with
-        # that fields on the payment form before it goes to PayPal)
-        if ipn_obj.receiver_email != "sb-o81vn645225@business.example.com":
-            # Not a valid payment
-            return
-
-        # ALSO: for the same reason, you need to check the amount
-        # received, `custom` etc. are all what you expect or what
-        # is allowed.
-
-        # Undertake some action depending upon `ipn_obj`.
-        if ipn_obj.custom == "basic_plan":
-            price = "1.10"
-        elif ipn_obj.custom == "standard_plan":
-            price = "2.20"
-        elif ipn_obj.custom == "custom_plan":
-            price = "3.30"
-
-        if ipn_obj.mc_gross == price and ipn_obj.mc_currency == 'CAD':
-            t = User.objects.get(username=request.user)
-            initial = t.profile.tokens
-            final = t.profile.tokens + 3
-            t.profile.tokens = t.profile.tokens + 3  # change field
-            t.profile.invoice_count = t.profile.invoice_count + 3
-            t.save() # this will update only
-    else:
-        return
-
-valid_ipn_received.connect(show_me_the_money)
+# from paypal.standard.models import ST_PP_COMPLETED
+# from paypal.standard.ipn.signals import valid_ipn_received
+#
+# def show_me_the_money(sender, **kwargs):
+#     ipn_obj = sender
+#     if ipn_obj.payment_status == ST_PP_COMPLETED:
+#         # WARNING !
+#         # Check that the receiver email is the same we previously
+#         # set on the `business` field. (The user could tamper with
+#         # that fields on the payment form before it goes to PayPal)
+#         if ipn_obj.receiver_email != "sb-o81vn645225@business.example.com":
+#             # Not a valid payment
+#             return
+#
+#         # ALSO: for the same reason, you need to check the amount
+#         # received, `custom` etc. are all what you expect or what
+#         # is allowed.
+#
+#         # Undertake some action depending upon `ipn_obj`.
+#         if ipn_obj.custom == "basic_plan":
+#             price = "1.10"
+#         elif ipn_obj.custom == "standard_plan":
+#             price = "2.20"
+#         elif ipn_obj.custom == "custom_plan":
+#             price = "3.30"
+#
+#         if ipn_obj.mc_gross == price and ipn_obj.mc_currency == 'CAD':
+#             t = User.objects.get(username=request.user)
+#             initial = t.profile.tokens
+#             final = t.profile.tokens + 3
+#             t.profile.tokens = t.profile.tokens + 3  # change field
+#             t.profile.invoice_count = t.profile.invoice_count + 3
+#             t.save() # this will update only
+#     else:
+#         return
+#
+# valid_ipn_received.connect(show_me_the_money)
