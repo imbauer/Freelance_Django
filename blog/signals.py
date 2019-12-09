8bd4426f7b26ff0f93897ccad41bccd476bda7a9
imbauer <ivanbauer16@gmail.com>
2019-12-08 23:14:06 -0500

    Testing message

--- blog/signals.py
+++ blog/signals.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from paypal.standard.ipn.signals import valid_ipn_received, invalid_ipn_received
+from django.dispatch import receiver
 
 @receiver(valid_ipn_received)
 def show_me_the_money(sender, **kwargs):
