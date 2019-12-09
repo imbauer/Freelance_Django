984b34380324e6708db8f7a714c976e6cbcac8b1
imbauer <ivanbauer16@gmail.com>
2019-12-09 01:38:39 -0500

    Testing message

--- blog/hooks.py
+++ blog/hooks.py
@@ -3,13 +3,33 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 
 @receiver(valid_ipn_received)
-def show_me_the_money(request, sender, **kwargs):
-    t = User.objects.get(username=request.user)
+def show_me_the_money(sender, **kwargs):
+    t = User.objects.get(username="vanya7")
 
-    initial = t.profile.tokens
-    final = t.profile.tokens + 10
-    t.profile.tokens = t.profile.tokens + 10  # change field
-    t.profile.invoice_count = t.profile.invoice_count + 10
+
+    if sender.custom == "basic_plan":
+        initial = t.profile.tokens
+        final = t.profile.tokens + 1
+        t.profile.tokens = t.profile.tokens + 1  # change field
+        t.profile.invoice_count = t.profile.invoice_count + 1
+
+    elif sender.custom == "standard_plan":
+        initial = t.profile.tokens
+        final = t.profile.tokens + 2
+        t.profile.tokens = t.profile.tokens + 2  # change field
+        t.profile.invoice_count = t.profile.invoice_count + 1
+
+    elif sender.custom == "custom_plan":
+        initial = t.profile.tokens
+        final = t.profile.tokens + 3
+        t.profile.tokens = t.profile.tokens + 3  # change field
+        t.profile.invoice_count = t.profile.invoice_count + 1
+
+    else:
+        initial = t.profile.tokens
+        final = t.profile.tokens + 5
+        t.profile.tokens = t.profile.tokens + 5  # change field
+        t.profile.invoice_count = t.profile.invoice_count + 1
 
     t.save() # this will update only
 
