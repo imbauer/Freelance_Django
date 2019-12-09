5201203aa464855480d470df860b77b08857ea79
imbauer <ivanbauer16@gmail.com>
2019-12-08 23:12:49 -0500

    Testing message

--- blog/signals.py
+++ blog/signals.py
@@ -4,10 +4,10 @@ from paypal.standard.ipn.signals import valid_ipn_received, invalid_ipn_received
 
 @receiver(valid_ipn_received)
 def show_me_the_money(sender, **kwargs):
-    messages.success(request, f'Increased from {initial} tokens to {final} tokens (+2)')
+    messages.success(request, f'SHOW MONEY')
     return HttpResponseRedirect('/pricing')
 
 @receiver(invalid_ipn_received)
 def do_not_show_me_the_money(sender, **kwargs):
-    messages.success(request, f'Increased from {initial} tokens to {final} tokens (+2)')
+    messages.success(request, f'DO NOT SHOW MONEY')
     return HttpResponseRedirect('/paypal-cancel')
