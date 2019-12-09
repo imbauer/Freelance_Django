b78cad37a745dc7f7aabb7855cee8f08e99ec292
imbauer <ivanbauer16@gmail.com>
2019-12-09 00:29:49 -0500

    Testing message

--- blog/hooks.py
+++ blog/hooks.py
@@ -1,36 +1,23 @@
-from paypal.standard.models import ST_PP_COMPLETED
-from paypal.standard.ipn.signals import valid_ipn_received
+from paypal.standard.ipn.signals import valid_ipn_received, invalid_ipn_received
+from django.contrib.auth.models import User
 
+@receiver(valid_ipn_received)
 def show_me_the_money(sender, **kwargs):
+    t = User.objects.get(username="TestUser")
+
+    initial = t.profile.tokens
+    final = t.profile.tokens + 10
+    t.profile.tokens = t.profile.tokens + 10  # change field
+    t.profile.invoice_count = t.profile.invoice_count + 10
+
+    t.save() # this will update only
+    
+    print("show_me_the_money: ")
     ipn_obj = sender
-    if ipn_obj.payment_status == ST_PP_COMPLETED:
-        # WARNING !
-        # Check that the receiver email is the same we previously
-        # set on the `business` field. (The user could tamper with
-        # that fields on the payment form before it goes to PayPal)
-        if ipn_obj.receiver_email != "sb-o81vn645225@business.example.com":
-            # Not a valid payment
-            return
+    print("show_me_the_money: " + ipn_obj.payment_status)
 
-        # ALSO: for the same reason, you need to check the amount
-        # received, `custom` etc. are all what you expect or what
-        # is allowed.
-
-        # Undertake some action depending upon `ipn_obj`.
-        if ipn_obj.custom == "basic_plan":
-            price = "1.10"
-            print("Small")
-        elif ipn_obj.custom == "standard_plan":
-            price = "2.20"
-            print("Medium")
-        elif ipn_obj.custom == "custom_plan":
-            price = "3.30"
-            print("Large")
-
-        if ipn_obj.mc_gross == price and ipn_obj.mc_currency == 'CAD':
-            print("Final Works")
-    else:
-        print("Did not work")
-        return
-
-valid_ipn_received.connect(show_me_the_money)
+@receiver(invalid_ipn_received)
+def do_not_show_me_the_money(sender, **kwargs):
+    print("none: ")
+    ipn_obj = sender
+    print("none: " + ipn_obj.payment_status)
