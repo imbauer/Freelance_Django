842926619a046ddc6224036bc3a22252fee6fb1f
imbauer <ivanbauer16@gmail.com>
2019-12-09 03:43:45 -0500

    Testing message

--- blog/static/blog/main.js
+++ blog/static/blog/main.js
@@ -15,7 +15,7 @@ function increaseValue() {
   document.getElementById('dynamic').innerHTML = a[0];
   document.getElementById('dynamicdec').innerHTML = "." + parseFloat(a[1]).toFixed(2).toString().slice(-2);
   var input = document.getElementById("cost");
-  input.value = value.toString() + ".00";
+  input.value = count;
   console.log(input.value);
 }
 
