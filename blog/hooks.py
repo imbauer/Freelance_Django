fcf8aafcbc6504e76915599bafbeb3a245d559df
imbauer <ivanbauer16@gmail.com>
2019-12-09 00:31:15 -0500

    Testing message

--- blog/hooks.py
+++ blog/hooks.py
@@ -1,5 +1,6 @@
 from paypal.standard.ipn.signals import valid_ipn_received, invalid_ipn_received
 from django.contrib.auth.models import User
+from django.dispatch import receiver
 
 @receiver(valid_ipn_received)
 def show_me_the_money(sender, **kwargs):
@@ -11,7 +12,7 @@ def show_me_the_money(sender, **kwargs):
     t.profile.invoice_count = t.profile.invoice_count + 10
 
     t.save() # this will update only
-    
+
     print("show_me_the_money: ")
     ipn_obj = sender
     print("show_me_the_money: " + ipn_obj.payment_status)
