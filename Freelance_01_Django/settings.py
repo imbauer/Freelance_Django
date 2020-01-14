21f6677de80d9b64450cb9b0fb47d50ac6f9910c
imbauer <ivanbauer16@gmail.com>
2020-01-14 03:49:17 -0500

    Removed RDS errors

--- Freelance_01_Django/settings.py
+++ Freelance_01_Django/settings.py
@@ -221,11 +221,11 @@ WSGI_APPLICATION = 'Freelance_01_Django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['RDS_HOSTNAME'],
-        'PORT': os.environ['RDS_PORT'],
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
