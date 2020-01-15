3575b83f27130207fa09b515eef8944f501dfb37
imbauer <ivanbauer16@gmail.com>
2020-01-15 03:41:10 -0500

    fixing static files

--- Freelance_01_Django/settings.py
+++ Freelance_01_Django/settings.py
@@ -265,8 +265,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'blog/static/blog'),
+  os.path.join(BASE_DIR, 'blog/static/blog/css'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
