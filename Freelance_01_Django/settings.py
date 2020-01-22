6e7312f1c7745a95f3c5daf8048a4795c59211da
imbauer <ivanbauer16@gmail.com>
2020-01-22 02:12:32 -0500

    Changed RDS config

--- Freelance_01_Django/settings.py
+++ Freelance_01_Django/settings.py
@@ -161,7 +161,7 @@ SECRET_KEY = 'e^b6$r8ro9uwdf)jc4-()u(5v8sg=)*!xk!ehi)qnyaw(^hjqm'
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['freelance-01-django-dev.us-east-2.elasticbeanstalk.com']
 PAYPAL_TEST = True
@@ -267,15 +267,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
-STATIC_ROOT = os.path.join(BASE_DIR, 'blog/static/')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'blog/static/')
-]
-
-# STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-# STATIC_ROOT = 'static'
+STATIC_ROOT = 'static'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
