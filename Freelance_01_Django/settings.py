63bf9417403bba0ff43a1b1b91944376b04486f2
imbauer <ivanbauer16@gmail.com>
2020-01-22 03:35:59 -0500

    Changed RDS config

--- Freelance_01_Django/settings.py
+++ Freelance_01_Django/settings.py
@@ -269,7 +269,7 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
