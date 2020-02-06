3af4b139eb624b3dbf6a7a858402eb6eb6e03035
Ivan Bauer <ivanbauer16@gmail.com>
2020-02-06 13:57:57 -0500

    Set db configs

--- Freelance_01_Django/settings.py
+++ Freelance_01_Django/settings.py
@@ -221,10 +221,10 @@ WSGI_APPLICATION = 'Freelance_01_Django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ebdb',
+        'NAME': 'aa1b40ewnktgym7',
         'USER': 'postgres',
         'PASSWORD': 'canadawaterbottle^23',
-        'HOST': 'aal6kveaaivm37.cgvklsytn3bv.us-east-2.rds.amazonaws.com',
+        'HOST': 'aa1b40ewnktgym7.cgvklsytn3bv.us-east-2.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
